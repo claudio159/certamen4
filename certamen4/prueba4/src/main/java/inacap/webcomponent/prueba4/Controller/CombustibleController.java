29b0410eeb240b968ae19c8abd50ded0620631b8
claudio159 <40439814+claudio159@users.noreply.github.com>
2018-06-29 18:14:12 -0400

    Add files via upload
    avance dia 29

---
+++ certamen4/prueba4/src/main/java/inacap/webcomponent/prueba4/Controller/CombustibleController.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inacap.webcomponent.prueba4.Controller;
+
+import inacap.webcomponent.prueba4.Model.CombustibleModel;
+import inacap.webcomponent.prueba4.Repository.CombustibleRepository;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+/**
+ *
+ * @author Seba
+ */
+@RestController
+@RequestMapping("/url")
+public class CombustibleController {
+    
+    @Autowired
+    private CombustibleRepository combustibleRepository;
+      
+    
+    @GetMapping()
+    public Iterable<CombustibleModel> listarTodos() {
+        
+        return combustibleRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity <CombustibleModel>muestrcombustiblea (@PathVariable String id) {
+        
+        Optional<CombustibleModel>cOptional = combustibleRepository.findById(Integer.parseInt(id));
+        
+        if(cOptional.isPresent()){
+            return new ResponseEntity(cOptional.get(), HttpStatus.FOUND);
+        }
+        else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<CombustibleModel> editarcombustible(@PathVariable String id, @RequestBody CombustibleModel CombustibleEditar) {
+        
+        Optional<CombustibleModel> aOptional = combustibleRepository.findById(Integer.parseInt(id));
+        
+        if(aOptional.isPresent()){
+            CombustibleModel cEncontrado = aOptional.get();
+            
+            CombustibleEditar.setIdCombustible(cEncontrado.getIdCombustible());
+            
+            combustibleRepository.save(CombustibleEditar);
+            
+            return new ResponseEntity<>(CombustibleEditar, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_MODIFIED);
+        }
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> agregarCombustible(@RequestBody CombustibleModel nuevoCombustible) {
+        
+        nuevoCombustible = combustibleRepository.save(nuevoCombustible);
+        
+        Optional<CombustibleModel> cOptional = combustibleRepository.findById(nuevoCombustible.getIdCombustible());
+        
+        if(cOptional.isPresent()){
+            return new ResponseEntity<>(cOptional.get(), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+       
+        Optional<CombustibleModel> cOptional = combustibleRepository.findById(Integer.parseInt(id));
+        
+        if(cOptional.isPresent()){
+            combustibleRepository.deleteById(Integer.parseInt(id));
+            return new ResponseEntity<>(cOptional.get(), HttpStatus.OK);
+        }else{
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }    
+    }
+    
+    
+}
---
+++ certamen4/prueba4/src/main/java/inacap/webcomponent/prueba4/Model/CombustibleModel.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inacap.webcomponent.prueba4.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Seba
+ */
+@Entity
+@Table(name = "combustible")
+public class CombustibleModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private int idCombustible;
+    private String tipoCombustible;
+    private String detalle;
+
+    public int getIdCombustible() {
+        return idCombustible;
+    }
+
+    public void setIdCombustible(int idCombustible) {
+        this.idCombustible = idCombustible;
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public CombustibleModel() {
+    }
+
+    public CombustibleModel(String tipoCombustible, String detalle) {
+        this.tipoCombustible = tipoCombustible;
+        this.detalle = detalle;
+    }
+
+    public CombustibleModel(int idCombustible, String tipoCombustible, String detalle) {
+        this.idCombustible = idCombustible;
+        this.tipoCombustible = tipoCombustible;
+        this.detalle = detalle;
+    }
+    
+    
+    
+    
+    
+}
